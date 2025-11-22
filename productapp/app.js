712b6bab46ea17ab7a3bc69e248892fa1e65c040
wisdm-nikhil <nikhil.mhaske@gmail.com>
2025-11-23 01:58:36 +0530

    App Upload Done

--- productapp/app.js
+++ productapp/app.js
@@ -20,12 +20,19 @@ app.listen(3000, () => {
 });
 
 
+// HOW TO RUN THIS PROGRAM:
+
+// REQUIRED SETUP (Run these once if not already done):
+// npm init -y
+// npm install express
+// npm install
+
 // HOW TO RUN THIS APPLICATION:
 // Step 1: Open terminal inside this folder
 // Step 2: Run → node app.js
 // Step 3: Open browser → http://localhost:3000/products (GET request)
 
-// HOW TO TEST CREATE REQUEST (POSTMAN OR TERMINAL):
+// Open New Terminal and go to file using cd filename 
 // Example Terminal Command for Adding Product:
 // curl -X POST -H "Content-Type: application/json" -d '{"name":"Laptop","price":45000}' http://localhost:3000/products
 
